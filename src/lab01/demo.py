86ee181e0054b23d756f6898db027f34aebdda1f
David Laurindo <davidlaurindo05@gmail.com>
2026-03-13 14:44:14 +0100

    LAB01_TRANSLATED_OUTPUT

--- src/lab01/demo.py
+++ src/lab01/demo.py
@@ -8,19 +8,19 @@ def run_demo():
     try:
         invalid_product = Product("", "Lenovo-Thinkpad", 1200.0, 10)  # Empty name
     except ValueError as e:
-        print(f"Captura de erro esperada: {e}") #Name cannot be empty
+        print(f"Ожидаемый перехват ошибок:: {e}") #Name cannot be empty
     try:
         invalid_product = Product("Printer", "   ", 1200.0, 10)  # Model's name
     except ValueError as e:
-        print(f"Captura de erro esperada: {e}") #Model must have at least 3 chars    
+        print(f"Ожидаемый перехват ошибок:: {e}") #Model must have at least 3 chars    
     try:
         invalid_product = Product("Printer", "Lenovo-Thinkpad", -4, 10)  # Invalid price
     except ValueError as e:
-        print(f"Captura de erro esperada: {e}") #Price must be at least 0 (means free product - eg, marketing blackfriday)
+        print(f"Ожидаемый перехват ошибок:: {e}") #Price must be at least 0 (means free product - eg, marketing blackfriday)
     try:
         invalid_product = Product("Printer", "Lenovo-Thinkpad", 1200.0, -4)  # Invalid stock value
     except ValueError as e:
-        print(f"Captura de erro esperada: {e}") #stock must begin from 0
+        print(f"Ожидаемый перехват ошибок:: {e}") #stock must begin from 0
 
     # Demonstração de Sucesso e Estado
     inventory = [
@@ -29,23 +29,23 @@ def run_demo():
         Product("Keyboard OLED", "Ajazz AK820 Pro", 24305.00, 50),
         Product("Laptop Pro", "Lenovo-Thinkpad", 1200.0, 10) # Duplicado (mesmo nome e modelo)
     ]
-    print("\n--------Comparação de produtos-------------")
+    print("\n--------Comparation of products-------------")
     for i in range(len(inventory)): #i pega os 4 elementos. A iteração começa com i=0
         for j in range(i + 1, len(inventory)): # j pega i+1 até 4-3, ou seja sai de i+1 até 3
             # O método __eq__ será chamado aqui e disparará o Warning se forem iguais
             if inventory[i] == inventory[j]: #Primeia iteração, por exemplo, compara se item[0]==item[1]
-                print(f"   -> Ação sugerida: Unificar stock dos itens {i+1} e {j+1}.\n")#+1, por causa do utilizador.
+                print(f"   -> Предлагаемое действие: Унифицировать складские запасы товаров. {i+1} и {j+1}.\n")#+1, por causa do utilizador.
 
     #Reorganização dos produtos, depois de haver feito a comparação em nome e modelo.
     p1 = inventory[0]  # Laptop Pro Lenovo-Thinkpad
     p2 = inventory[1]  # Mouse RGB Logitech G502 X PLUS
     p3 = inventory[2]  # Keyboard OLED Ajazz AK820 Pro
 
-    print(f"==================\nProdutos Criados: \n{p1}\n{p2}\n{p3}\n") #Vista Usuário
-    print(f"==================\nProdutos Criados (Para desenvolvedor): \n{repr(p1)}\n{repr(p2)}\n{repr(p3)}") #Vista programador
+    print(f"==================\nCreated Products: \n{p1}\n{p2}\n{p3}\n") #Vista Usuário
+    print(f"==================\nCreated Products (For developers): \n{repr(p1)}\n{repr(p2)}\n{repr(p3)}") #Vista programador
 
     
-    print("\n------------2. Testando Comportamento de Estado:----------------")
+    print("\n------------2. Проверка поведения состояния:----------------")
     p1.deactivate()
     p1.sell(1) # Deve ser bloqueado por estar inativo
     p1.activate()
@@ -63,12 +63,12 @@ def run_demo():
     Product.store_name="PROLTCH ANGOLA, Lda" #Nome da loja mudado para "PROLTCH ANGOLA, Lda"
     # Alteração via atributo de instância, alteração local - afecta paneas o produto
     p1.store_name="David" #Produto p1 é de uma loja/fabricante diferente
-    print(f"Via Class: {Product.store_name}") #PROLTCH ANGOLA, Lda
-    print(f"Via Instance p3: {p3.store_name}") #PROLTCH ANGOLA, Lda
-    print(f"Via Instance p1: {p1.store_name}") #David
+    print(f"Через класс: {Product.store_name}") #PROLTCH ANGOLA, Lda
+    print(f"Через экземпляр p3: {p3.store_name}") #PROLTCH ANGOLA, Lda
+    print(f"Через экземпляр p1: {p1.store_name}") #David
     
     #Produts updated list/status
-    print(f"\n==================\nProdutos: \n{p1}\n{p2}\n{p3}")
+    print(f"\n==================\Продукты: \n{p1}\n{p2}\n{p3}")
 
 if __name__ == "__main__":
     run_demo()
--- src/libraries/validation.py
+++ src/libraries/validation.py
@@ -3,7 +3,7 @@ def validate_non_empty_string(value, field_name="Campo"):
     Garante que as strings não estejam vazias.
     """
     if not isinstance(value, str) or not value.strip():
-        raise ValueError(f"Erro de Validação: {field_name} deve ser uma string não vazia.")
+        raise ValueError(f"Validation Error: {field_name} must be a non-empty string.")
     return value.strip()
 
 def validate_positive_number(value, field_name="Valor"):
@@ -11,7 +11,7 @@ def validate_positive_number(value, field_name="Valor"):
     Garante que valores numéricos sejam todos positivos, incluindo o zero.
     """
     if not isinstance(value, (int, float)) or value < 0:
-        raise ValueError(f"Erro de Validação: {field_name} deve ser um número >= 0.")
+        raise ValueError(f"Validation Error: {field_name} must be a number >= 0.")
         #preços iguais à 0: possibilidade de produtos grátis (promoção de marketing ou cliente bonificado)
     return float(value)
 
@@ -20,7 +20,7 @@ def validate_positive_int(value, field_name="Quantidade"):
     Garante que valores numéricos inteiros sejam todos positivos, incluindo o zero.
     """
     if not isinstance(value, int) or value < 0:
-        raise ValueError(f"Erro de Validação: {field_name} deve ser um número inteiro >= 0.")
+        raise ValueError(f"Validation Error: {field_name} must be an integer >= 0.")
     return value
 
 def validate_min_length(value, min_len, field_name="Texto"):
@@ -28,5 +28,5 @@ def validate_min_length(value, min_len, field_name="Texto"):
     Garante que string tenha um mínimo de caractere considerável.
     """
     if not isinstance(value, str) or len(value.strip()) < min_len:
-        raise ValueError(f"Erro de Validação: {field_name} deve ter pelo menos {min_len} caracteres.")
+        raise ValueError(f"Validation Error: {field_name} must have at least {min_len} characters.")
     return value.strip()
